dea53c50a407b7238297b0a31dfa4f58e45d5a7f
khalidsharmarke <ksharmarke972@gmail.com>
2021-09-09 16:27:32 -0400

    changed server routing

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 8800;
 
-app.use('', express.static('static'));
+app.use('/', express.static('static'));
 
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
